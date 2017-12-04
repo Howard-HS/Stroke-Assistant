c735103714ed8e3a05a8faf1f090db177da4cdf5
Kah Chun Siah <john_960312@hotmail.com>
2017-12-04 11:39:33 +0800

    Add button & drag and drop library

--- app/src/main/java/aad/assignment/strokeassistant/PredefinedTextActivity.java
+++ app/src/main/java/aad/assignment/strokeassistant/PredefinedTextActivity.java
@@ -2,6 +2,7 @@ package aad.assignment.strokeassistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class PredefinedTextActivity extends AppCompatActivity {
@@ -14,10 +15,19 @@ public class PredefinedTextActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.predefined_text, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
         if(id == android.R.id.home) finish();
+        else if(id == R.id.action_add_predefined_text) {
+
+        }
 
         return super.onOptionsItemSelected(item);
     }
